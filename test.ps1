24be9aab69faa28152f4d466d071b547137ba506
Daniel Przybylski <danp43@outlook.com>
2022-01-19 15:27:59 -0800

    Add demo script

---
+++ test.ps1
@@ -0,0 +1,2 @@
+
+Write-Demo -Subject "To whom it may concern" -Tos "Larry, Curly, Moe" -Bccs "Manny, Moe, Jack"  -What "A test error occured" -Reason "You have received this because dog-gone it, people like you!"
